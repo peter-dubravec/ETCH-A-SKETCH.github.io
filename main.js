e435595df839fed4a6e7b7aa9c2d985836b26cb7
Peter Dubravec <peter.dubravec98@gmail.com>
2021-10-06 23:25:42 +0200

    finished Odin Project Etch-A-Sketch

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 //creating 16x16 grid
-
-let container = document.createElement("div");
-container.classList = "container";
-
-document.body.appendChild(container);
+let wrapper = document.querySelector(".wrapper");
+let buttonNewGrid = document.querySelector(".buttonNewGrid");
 
 function addDiv(i) {
   let div = `div${i}`;
@@ -11,31 +8,62 @@ function addDiv(i) {
   return createdDiv;
 }
 
-function createGrid() {
-  for (let i = 0; i < 16; i++) {
+function createGrid(numOfSquares, squaresWidth, option = "default") {
+  let container = document.createElement("div");
+  container.classList = "container";
+  wrapper.insertBefore(container, buttonNewGrid);
+
+  // wrapper.appendChild(container);
+  for (let i = 0; i < numOfSquares; i++) {
     let div = addDiv(i);
-    div.className = "divGrid";
+    div.className = "row";
     container.appendChild(div);
 
-    for (let j = 0; j < 16; j++) {
-      let insideDiv = document.createElement("div");
-      insideDiv.classList = "insideDiv";
-      div.appendChild(insideDiv);
+    for (let j = 0; j < numOfSquares; j++) {
+      let columnDiv = document.createElement("div");
+      columnDiv.classList = "columnDiv";
+      columnDiv.style.width = squaresWidth + "px";
+      columnDiv.style.height = squaresWidth + "px";
+      div.appendChild(columnDiv);
     }
   }
+  let divHover = document.querySelectorAll(".columnDiv");
+  divHover.forEach((element) => {
+    element.addEventListener("mouseenter", (e) => {
+      e.target.classList.add("changeToRed");
+    });
+  });
 }
+// initial setting
+let numOfSquares = 16;
+let squaresWidth = 35;
 
-createGrid();
+createGrid(numOfSquares, squaresWidth);
 
-// hovering effect
+// New blank grid after clicking button
 
-let divHover = document.querySelectorAll(".insideDiv");
-
-divHover.forEach((element) => {
-  element.addEventListener("mouseenter", (e) => {
-    e.target.classList.add("changeToRed");
+buttonNewGrid.addEventListener("click", () => {
+  let columnDiv = document.querySelectorAll(".columnDiv");
+  columnDiv.forEach((div) => {
+    div.classList.remove("changeToRed");
   });
-  element.addEventListener("animationend", (e) => {
-    e.target.classList.remove("changeToRed");
-  });
+});
+
+// Button to change numOfSquares
+
+const buttonNumOfSquares = document.querySelector(".buttonNumOfSquares");
+buttonNumOfSquares.addEventListener("click", () => {
+  let container = document.querySelector(".container");
+  let numOfSquares = prompt("Please enter a number of squares");
+  console.log(numOfSquares);
+  if (!numOfSquares) {
+    return;
+  }
+  if (numOfSquares > 100) {
+    alert("Please enter number that is less than 100");
+    return;
+  }
+  let squaresWidth = parseFloat(560 / numOfSquares).toFixed(2);
+  wrapper.removeChild(container);
+  createGrid(numOfSquares, squaresWidth);
 });
